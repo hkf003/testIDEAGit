5dd5c8c81cfd65a35aaa053d96dc3f7b764ecb1b
hkfjava <75961995+hkfjava@users.noreply.github.com>
2020-12-14 18:14:16 +0800

    新增的beanPerson

---
+++ src/main/java/com/atguigu/bean/Person.java
@@ -0,0 +1,22 @@
+package com.atguigu.bean;
+
+public class Person {
+    private String pName;
+    private String pPhone;
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpPhone() {
+        return pPhone;
+    }
+
+    public void setpPhone(String pPhone) {
+        this.pPhone = pPhone;
+    }
+}
